08c04c82debbca11e690db5e281148944a1631de
danieljimenez0255 <42664403+danieljimenez0255@users.noreply.github.com>
2020-09-18 13:18:05 -0700

    util js

--- src/util.js
+++ src/util.js
@@ -12,7 +12,7 @@ const casesTypeColors = {
     multiplier: 1200,
   },
   deaths: {
-    hex: "#fb4443",
+    hex: "#87CEFA",
     multiplier: 2000,
   },
 };
@@ -60,3 +60,81 @@ export const showDataOnMap = (data, casesType = "cases") =>
       </Popup>
     </Circle>
   ));
+
+// build chart data(change in cases each day)
+export const buildChartData = (data, casesType) => {
+  const chartData = [];
+  let lastDataPoint = 0;
+
+  for (let date in data[casesType]) {
+    if (lastDataPoint) {
+      let newDataPoint = {
+        x: date,
+        y: data[casesType][date] - lastDataPoint, //how many new cases appeared on new date from last date
+      };
+      chartData.push(newDataPoint);
+    }
+
+    lastDataPoint = data[casesType][date];
+  }
+  return chartData;
+};
+
+//overall total cases each day
+export const totalChartData = (data) => {
+  const chartData = [];
+  for (let date in data.cases) {
+    let newDataPoint = {
+      x: date,
+      y: data.cases[date],
+    };
+    chartData.push(newDataPoint);
+  }
+  return chartData;
+};
+
+// line chart options
+export const options = {
+  legend: {
+    display: false,
+  },
+  elements: {
+    point: {
+      radius: 0,
+    },
+  },
+  maintainAspectRatio: false,
+  responsive: true,
+  tooltips: {
+    mode: "index",
+    intersect: false,
+    callbacks: {
+      label: function (tooltipItem, data) {
+        return numeral(tooltipItem.value).format("+0,0");
+      },
+    },
+  },
+  scales: {
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          parse: "MM/DD/YY",
+          tooltipFormat: "ll",
+        },
+      },
+    ],
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return numeral(value).format("0a");
+          },
+        },
+      },
+    ],
+  },
+};
